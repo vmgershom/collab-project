998095b707cef55d3b9fc59049e209ba5dd88a11
vmgershom01 <vmgershom@gmail.com>
2026-06-03 14:43:22 +0300

    CritMatrix update

--- src/utils/criteriaMatrix.js
+++ src/utils/criteriaMatrix.js
@@ -1,10 +1,10 @@
 const { priorityVector, consistency } = require('./ahp');
 
 const CRITERIA_MATRIX = [
-  [1, 3, 7, 4],
-  [1 / 3, 1, 4, 2],
-  [1 / 7, 1 / 4, 1, 1 / 3],
-  [1 / 4, 1 / 2, 3, 1],
+  [1, 2, 4, 4],
+  [1 / 2, 1, 2, 2],
+  [1 / 4, 1 / 2, 1, 1 / 2],
+  [1 / 4, 1 / 2, 2, 1],
 ];
 
 const CRITERIA_WEIGHTS = priorityVector(CRITERIA_MATRIX);
